a9bb0d8573d02e1ff16c1cda4c24903d1376ac1a
MariaSanchezPortillo <msanchezpo@unal.edu.co>
2025-10-24 11:33:42 -0500

    Refactor comments and improve code clarity

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#IMPORTAMOS TODOS LOS ANIMALES
 from animal import Animal
 from model.leon import Leon
 from model.aguila import Aguila
@@ -5,11 +6,11 @@ from model.paloma import Paloma
 from model.gato import Gato
 from model.perro import Perro
 
-# 2. ¡IMPORTAMOS EL SERVICIO DE FIREBASE! (CORREGIDO)
+#IMPORTAMOS EL SERVICIO DE FIREBASE
 from firebase_config import FirebaseRealtime  
 
 try:
-    # 3. INICIALIZAMOS LA CONEXIÓN A FIREBASE
+    #INICIALIZAMOS LA CONEXIÓN A FIREBASE
     print("Conectando a Firebase...")
     db_service = FirebaseRealtime(base_path="refugio_animal")
 
@@ -21,26 +22,24 @@ try:
     gato1 = Gato(masa=3, especie="gato", voz="miaauuu", personalidad="cariñoso")
     paloma1 = Paloma(masa=0.5, especie="paloma", voz="cu curru cu cú", color="gris")
 
+    #CREAMOS LISTA CON TODOS LOS ANIMALES
     refugio_animal = [aguila1, leon1, perro1, gato1, paloma1]
 
     print("\n--- Guardando en Firebase ---")
     for animal in refugio_animal:
         db_service.save_animal(animal)
 
-    # 6. Tu demostración de Polimorfismo (¡perfecto!)
+    #DEMOSTRACIÓN DE POLIMORFISMO
     print("\n--- Demostración de Polimorfismo (Consola) ---")
     for animal in refugio_animal:
         animal.reproducir_voz()
         animal.dormir()
 
-    # --- ¡ERROR 3 CORREGIDO! ---
-    # Movimos este bloque para que esté DENTRO del 'try'
     print("\n--- Demostración de Herencia (dormir) ---")
     for animal in refugio_animal:
-        animal.dormir()  # <-- Esto funciona gracias al ERROR 2 CORREGIDO
+        animal.dormir() 
 
 except Exception as e:
-    # Capturamos cualquier error (ej. si falla la conexión a Firebase)
     print(f"\n--- ERROR CRÍTICO ---")
     print(f"No se pudo ejecutar el programa: {e}")
     print("Revisa tu 'credentials.json' y la URL en 'firebase_service.py'")
